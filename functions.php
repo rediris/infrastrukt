e6fbf92547f74acca8393d5123d60f45ac0afbdc
Drew Morris <drewsymorris@Drew-Morris-MacBook-Pro.local>
2011-10-24 02:27:03 +0800

    Added CDN (Google) hosted jQuery instead of local copy. Will work on roll-back soon.

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+function my_scripts_method() {
+    wp_deregister_script( 'jquery' );
+    wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js');
+    wp_enqueue_script( 'jquery' );
+}    
+ 
+add_action('wp_enqueue_scripts', 'my_scripts_method');
+?>
