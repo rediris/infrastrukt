47053ba533a80ca6cc53fed194e2ba92e834da1c
Drew Morris <drewsy.morris@gmail.com>
2011-11-17 06:33:45 +0800

    Removed a few little things

--- Foundation, the Theme/searchform.php
+++
@@ -1,6 +0,0 @@
-<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-    <div><label class="screen-reader-text" for="s"><h4>Search</h4></label>
-        <input class="nice" type="text" value="" name="s" id="s" />
-        <input class="nice button radius" type="submit" id="searchsubmit" value="Search" />
-    </div>
-</form>
