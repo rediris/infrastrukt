52ddcac31ffa083ff94d96eb11748acb9b43248d
Drew Morris <drewsy.morris@gmail.com>
2011-11-18 09:45:26 +0800

    Added a post count and back to top link for each post (only applicable on mobile)

--- index.php
+++ index.php
@@ -18,39 +18,45 @@
 			
 				<article class="nine columns">
 				
-				<!-- Loop Through Posts -->
-				<?php while ( have_posts() ) : the_post(); ?>
-			
-			        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>  
+					<!-- Loop Through Posts -->
+					<?php query_posts( 'posts_per_page=2' ); ?>
+				
+					<?php while ( have_posts() ) : the_post(); ?>
 					
-	        		<div>
-	               		Posted on <strong><?php the_date(); ?></strong>
-	               		by <span class="author"><?php the_author(); ?></span>
-	                	under <span class="author"><?php the_category(', '); ?></span>  
-	                </div> 
-	                
-	                <div>
-						<?php edit_post_link('Edit', '<span>  ' , '</span>'); ?>
-						<span class="comment-count"><?php comments_popup_link('Leave a comment', '1 Comment', '% Comments'); ?></span>  
-					</div> 
+						<a href="#" class="hide-on-desktops" style="float: right;" alt="Back to Top">Top &uarr;</a>
+						
+				        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>  
+						
+		        		<div>
+		               		Posted on <strong><?php the_date(); ?></strong>
+		               		by <span class="author"><?php the_author(); ?></span>
+		                	under <span class="author"><?php the_category(', '); ?></span>	                	  
+		                </div> 
+		                
+		                <div>
+							<?php edit_post_link('Edit', '<span>  ' , '</span>'); ?>
+							<span class="comment-count"><?php comments_popup_link('Leave a comment', '1 Comment', '% Comments'); ?></span>  
+						</div> 
 	  			
-	        		<?php if ( is_archive() || is_search() ) : // Only display excerpts for archives and search. ?>  
-	            	<?php the_excerpt(); ?>  
-	        			<?php else : ?>  
-	            		<?php the_content('Read More'); ?>  
-	            		<hr>
-	        		<?php endif; ?>
-	        		
-	        		
-	        		<!-- End Looping Through Posts -->
-	        		
-	        		<?php endwhile; ?>  
+		        		<?php if ( is_archive() || is_search() ) : // Only display excerpts for archives and search. ?>  
+		            	<?php the_excerpt(); ?>  
+		        			<?php else : ?>  
+		            		<?php the_content('Read More'); ?>  
+		            		<hr>
+		        		<?php endif; ?>
+	     
+	        		<?php endwhile; ?>
+	        		<!-- End Looping Through Posts -->  	        		
 	        	
-	        	<!-- Begin Pagination -->
-	        	<?php if (function_exists("emm_paginate")) {
-	        	    emm_paginate();
-	        	} ?>
-	        	<!-- End Pagination -->
+		        	<!-- Begin Pagination -->
+		        	<?php if (function_exists("emm_paginate")) {
+		        	    emm_paginate();
+		        	} ?>	        	
+		        	<!-- End Pagination -->
+		        	
+		        	<!-- Reset Query -->
+		        	<?php wp_reset_query(); ?>
+		        	<!-- End Reset Query -->
 	        	
         		</article>
         		
