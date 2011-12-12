9de908a4ae9148778286f4c85821ec1d799a4adc
Drew Morris <drewsy.morris@gmail.com>
2011-12-12 20:31:19 +0800

    Fixed search box

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
     <div><label class="screen-reader-text" for="s">Search for:</label>
-        <input type="search" placeholder="Search" name="s" id="s" />
+        <input type="search" results="5" placeholder="Search" name="s" id="s" />
     </div>
 </form>
