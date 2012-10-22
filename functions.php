80ce3b9eb049d433c79c8e7b2a38be1a145442ad
Drew Morris <drewsy.morris@gmail.com>
2012-10-22 22:42:44 +1100

    Fixed whitespace issue
    Fixed whitespace issue

--- functions.php
+++ functions.php
@@ -146,9 +146,9 @@ $big = 999999999;
 $links = paginate_links( array(
 	'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
 	'format' => '?paged=%#%',
-    'prev_next' => true,
-    'prev_text' => '&laquo;',
-    'next_text' => '&raquo;',
+	'prev_next' => true,
+	'prev_text' => '&laquo;',
+	'next_text' => '&raquo;',
 	'current' => max( 1, get_query_var('paged') ),
 	'total' => $wp_query->max_num_pages,
 	'type' => 'list'
