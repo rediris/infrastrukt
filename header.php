bee92e6452d55d94e117af33b001619e69621319
Drew Morris <drewsymo@Drews-MacBook-Pro-2.local>
2013-04-09 23:18:12 +1000

    Updated header.php to F4

--- header.php
+++ header.php
@@ -11,12 +11,9 @@
 ?>
 
 <!DOCTYPE html>
-
-<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
-<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 8]>    <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en" > <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
+
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 
@@ -36,12 +33,12 @@
 
 	<header class="row">
 
-		<hgroup class="site-title twelve columns">
+		<hgroup class="site-title large-12 columns">
 			<h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h3 class="subheader"><?php bloginfo('description'); ?></h3>
 		</hgroup>
 
-		<?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'menu_class' => 'nav-bar', 'fallback_cb' => 'foundation_page_menu', 'container' => 'nav', 'container_class' => 'twelve columns', 'walker' => new foundation_navigation() ) ); ?>
+		<?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'menu_class' => 'nav-bar', 'fallback_cb' => 'foundation_page_menu', 'container' => 'nav', 'container_class' => 'large-12 columns', 'walker' => new foundation_navigation() ) ); ?>
 
 	</header>
 
