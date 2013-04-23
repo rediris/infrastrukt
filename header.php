760ca151f02adc7ddbfb594f3a4ae1be534ffdcc
Drew Morris <drewsymo@Drews-MacBook-Pro-2.local>
2013-04-23 22:36:55 +1000

    Header Fix

--- header.php
+++ header.php
@@ -31,28 +31,26 @@
 
 <body <?php body_class(); ?>>
 
-	<header>
+	<nav class="top-bar">
+		<ul class="title-area">
+			<li class="name"><h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo('name'); ?></a></h1></li>
+			<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+		</ul>
+		<section class="top-bar-section">
+			<?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'menu_class' => 'left', 'container' => '', 'fallback_cb' => 'foundation_page_menu', 'walker' => new foundation_navigation() ) ); ?>
+		</section>
+	</nav>
 
-		<nav class="top-bar">
-			<ul class="title-area">
-				<li class="name"><h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo('name'); ?></a></h1></li>
-				<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
-			</ul>
-			<section class="top-bar-section">
-				<?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'menu_class' => 'left', 'container' => '', 'fallback_cb' => 'foundation_page_menu', 'walker' => new foundation_navigation() ) ); ?>
-			</section>
-		</nav>
-
-		<?php $header =  get_header_textcolor();
-		if ( $header !== "blank" ) : ?>
-		<header class="site-header" <?php $header_image = get_header_image(); if ( ! empty( $header_image ) ) : ?> style="background:url('<?php echo esc_url( $header_image ); ?>');" <?php endif; ?>>
-			<div class="row">
-				<div class="large-12 columns">
-					<h2><a style="color:#<?php header_textcolor(); ?>;" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'description' ); ?></a></h2>
-				</div>
+	<?php $header =  get_header_textcolor();
+	if ( $header !== "blank" ) : ?>
+	<header class="site-header" <?php $header_image = get_header_image(); if ( ! empty( $header_image ) ) : ?> style="background:url('<?php echo esc_url( $header_image ); ?>');" <?php endif; ?>>
+		<div class="row">
+			<div class="large-12 columns">
+				<h2><a style="color:#<?php header_textcolor(); ?>;" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'description' ); ?></a></h2>
 			</div>
-		</header>
-		<?php endif; ?>
+		</div>
+	</header>
+	<?php endif; ?>
 
 <!-- Begin Page -->
 <div class="row">
