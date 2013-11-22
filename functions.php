4767063d88dc64b71be0be5c9ee049670482088e
rediris <romanedirisinghe+git@gmail.com>
2013-11-22 12:32:22 -0500

    Added Roots head cleanup

--- functions.php
+++ functions.php
@@ -428,4 +428,29 @@ if (file_exists($foundation_shortcodes)) {
 	require( $foundation_shortcodes );
 }
 
+/** 
+ * ADD ROOTS HEAD CLEANUP
+ * @see: http://benword.com/how-to-hide-that-youre-using-wordpress/
+ */
+
+if ( ! function_exists( 'roots_head_cleanup' ) ) :
+
+function roots_head_cleanup() {
+  remove_action('wp_head', 'feed_links', 2);
+  remove_action('wp_head', 'feed_links_extra', 3);
+  remove_action('wp_head', 'rsd_link');
+  remove_action('wp_head', 'wlwmanifest_link');
+  remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+  remove_action('wp_head', 'wp_generator');
+  remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+
+  global $wp_widget_factory;
+  remove_action('wp_head', array($wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style'));
+
+  add_filter('use_default_gallery_style', '__return_null');
+}
+
+add_action('init', 'roots_head_cleanup');
+endif;
+
 ?>
