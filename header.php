2533182f4915a918385d745884ea6d6f83299dc2
rediris <romanedirisinghe@gmail.com>
2013-12-09 00:01:11 -0500

    microdata fix via @https://github.com/84pixels/Foundation/commit/19d3b8eb995860b074bcad673efc38b1497de112

--- header.php
+++ header.php
@@ -42,18 +42,18 @@ endif;
 			<li class="name"><h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo('name'); ?></a></h1></li>
 			<li class="toggle-topbar menu-icon"><a href="#"><span><?php echo $topbar_menu_title; ?></span></a></li>
 		</ul>
-		<section class="top-bar-section">
-			<?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'menu_class' => 'left', 'container' => '', 'fallback_cb' => 'foundation_page_menu', 'walker' => new foundation_navigation() ) ); ?>
+		<section class="top-bar-section" role="navigation">
+			<?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'menu_class' => 'left', 'container' => '', 'fallback_cb' => 'infrastrukt_page_menu', 'walker' => new infrastrukt_navigation() ) ); ?>
 		</section>
 	</nav>
 </div><!--/.nav-wrapper-->
-	<?php $header =  get_header_textcolor();
-	if ( $header !== "blank" ) : ?>
-	<header class="site-header" <?php $header_image = get_header_image(); if ( ! empty( $header_image ) ) : ?> style="background:url('<?php echo esc_url( $header_image ); ?>');" <?php endif; ?>>
-		<div class="row">
-			<div class="large-12 columns">
-				<h2><a style="color:#<?php header_textcolor(); ?>;" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'description' ); ?></a></h2>
-			</div>
+<?php $header =  get_header_textcolor();
+if ( $header !== "blank" ) : ?>
+<header class="site-header" <?php $header_image = get_header_image(); if ( ! empty( $header_image ) ) : ?> style="background:url('<?php echo esc_url( $header_image ); ?>');" <?php endif; ?>>
+	<div class="row">
+		<div class="large-12 columns">
+			<h2><a style="color:#<?php header_textcolor(); ?>;" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'description' ); ?></a></h2>
 		</div>
-	</header>
-	<?php endif; ?>
+	</div>
+</header>
+<?php endif; ?>
