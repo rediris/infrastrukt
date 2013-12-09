854163845bbb0724523ef7b506d0281000c5cba8
rediris <romanedirisinghe@gmail.com>
2013-12-09 00:36:16 -0500

    commented out jquery enqueue

--- functions.php
+++ functions.php
@@ -65,12 +65,12 @@ function infrastrukt_js() {
 		/** 
 		 * DEREGISTER JQUERY
 		 */
-		wp_deregister_script('jquery');
+		//wp_deregister_script('jquery');
 
 		/** 
 		 * LOAD JS FROM CDN, IF POSSIBLE
 		 */
-		wp_enqueue_script( 'jquery-cdn', '//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js', array(), '2.0.3', true );
+		//wp_enqueue_script( 'jquery-cdn', '//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js', array(), '2.0.3', true );
 		wp_enqueue_script( 'jquery-migrate-cdn', '//code.jquery.com/jquery-migrate-1.2.1.min.js', array('jquery-cdn'), '1.2.1', true );
 		wp_enqueue_script( 'foundation-all', get_template_directory_uri().'/lib/foundation/js/foundation.all.min.js', array('jquery-migrate-cdn'), '5.0.0', true );
 		wp_enqueue_script( 'infrastrukt-js', get_template_directory_uri().'/js/infrastrukt.js', array('foundation-all'), '1.0', true);
