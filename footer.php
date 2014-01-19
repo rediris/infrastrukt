4071269c1c8ec83dec4026329d67465d82684850
rediris <romanedirisinghe@gmail.com>
2014-01-18 23:09:11 -0500

    dynamic footer update via @https://github.com/dougjohnston/Foundation/commit/c8860291eded74c78bac65284519d5c7919f7510

--- footer.php
+++ footer.php
@@ -13,15 +13,47 @@
 <!-- Footer -->
 <footer class="row">
 
-<?php if ( dynamic_sidebar('Sidebar Footer One') + dynamic_sidebar('Sidebar Footer Two') + dynamic_sidebar('Sidebar Footer Three') + dynamic_sidebar('Sidebar Footer Four')  ) : else : ?>
+<?php
+        $foundation_sidebars = array('foundation_sidebar_footer_one','foundation_sidebar_footer_two','foundation_sidebar_footer_three','foundation_sidebar_footer_four');
+        $count = 0;
+        /*
+                Check to see how many columns the footer is required to support
+                and unset inactive sidebars
+        */
+        foreach($foundation_sidebars as $x=>$sidebar)
+        {
+                if(is_active_sidebar($sidebar)) {
+                        $count++;
+                } else {
+                        unset($foundation_sidebars[$x]);
+                }
+
+        }
+        /*
+                Display active sidebars with the approprite column width
+                The widgets in the sidebars stack in their column
+        */
+        if($count){
+                $col_width = 12/$count;
+                foreach($foundation_sidebars as $sidebar)
+                {
+?>
+                <div class="large-<?php echo $col_width;?> columns">
+                        <?php dynamic_sidebar($sidebar); ?>
+                </div>
+<?php
+                }
+
+        } else {
+?>
 
 <div class="large-12 columns">
-	<ul class="inline-list">
-	<?php wp_list_pages('title_li='); ?>
-	</ul>
+        <ul class="inline-list">
+        <?php wp_list_pages('title_li='); ?>
+        </ul>
 </div>
 
-<?php endif; ?>
+<?php } ?>
 
 </footer>
 <!-- End Footer -->
